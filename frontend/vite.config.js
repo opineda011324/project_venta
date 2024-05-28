48ac76a0bfbbddf26a651ce8afa24efa884b9c12
opineda011324 <orlandopineda91989@gmail.com>
2024-05-28 17:56:38 -0500

    se actualiza en directo en front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // establece el directorio de salida de la compilación
+    outDir: 'dist',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
   },
 })
